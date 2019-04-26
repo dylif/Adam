aba7fd9a4f0743415ee3728972c77f03827c39cc
Dylan Lees <dylanml.home@gmail.com>
2019-04-26 12:02:27 -0400

    Apr 26 2019

--- i2c/basic_i2c.c
+++ i2c/basic_i2c.c
@@ -15,6 +15,9 @@ int i2c_read_reg8(int fd, int addr, uint8_t reg, uint8_t *buf)
 
 	if (fd < 0)
 		return fd;
+		
+	if (ioctl(fd, I2C_SLAVE, addr) < 0)
+		return errno * -1;
 
 	if (write(fd, &reg, reg_sz) != reg_sz)
 		return errno * -1;
@@ -29,61 +32,80 @@ int i2c_read_reg8(int fd, int addr, uint8_t reg, uint8_t *buf)
 int i2c_write_reg8(int fd, int addr, uint8_t reg, uint8_t data)
 {
 	uint8_t buf[2];
-	int sz = sizeof(buf) / sizeof(buf[0]);
+	int buf_sz = sizeof(buf) / sizeof(buf[0]);
 
 	if (fd < 0)
 		return fd;
+		
+	if (ioctl(fd, I2C_SLAVE, addr) < 0)
+		return errno * -1;
 
 	buf[0] = reg;
 	buf[1] = data;
 
-	if (write(fd, buf, sz) != sz)
+	if (write(fd, buf, buf_sz) != buf_sz)
 		return errno * -1;
 
-	return sz;
+	return buf_sz;
 }
 
 /* i2c_read_reg16: read a 16-bit int from a 8-bit device register */
 int i2c_read_reg16(int fd, int addr, uint8_t reg, uint16_t *buf)
 {
-	/* 8 bit register, 16 bit buffer */
-	int reg_sz = 1;
-	int buf_sz = 2;
+	int status;
 	
-	uint8_t cur_reg;
-
-	if (fd < 0)
-		return fd;
-
-	int i;
-	for (i = 0; i < 2; ++i) {
-		cur_reg = reg + i;
-		if (write(fd, &cur_reg, reg_sz) != reg_sz)
-			return  errno * -1;
-		if (read(fd, buf, buf_sz) != buf_sz)
-			return errno * -1;
-	}
+	uint8_t byte_buf[2];
+	int byte_buf_sz = sizeof(byte_buf) / sizeof(byte_buf[0]);
 	
-	return buf_sz;
+	if ((status = i2c_read(fd, addr, reg, byte_buf, byte_buf)) < 0)
+		return status;
+
+	*buf = (((uint16_t) byte_buf[0]) << 8) | ((uint16_t) byte_buf[1]);
+	
+	return byte_buf;
 }
 
 /* i2c_write_reg16: write a 16-bit int to a 8-bit device register */
 int i2c_write_reg16(int fd, int addr, uint8_t reg, uint16_t data)
 {
-	uint8_t buf[4];
-	int sz = sizeof(buf) / sizeof(buf[0]);
-
-	if (fd < 0)
-		return fd;
-
-	buf[0] = reg;
-	buf[1] = (uint8_t) (data & 0xFF);
-	buf[2] = reg + 1;
-	buf[3] = (uint8_t) (data >> 8);
+	int status; 
 	
+	uint8_t buf[2];
+	int buf_sz = sizeof(buf) / sizeof(buf[0]);
+	
+	buf[0] = (uint8_t) (data & 0xFF);
+	buf[1] = (uint8_t) (data >> 8);
 
-	if (write(fd, buf, sz) != sz)
-		return errno * -1;
+	if ((status = i2c_write(fd, addr, reg, buf, buf_sz)) < 0)
+		return status;
 
-	return sz;
+	return buf_sz;
+}
+
+/* i2c_read: Read multiple consecutive bytes from a i2c device. Reads no more than buf_sz bytes */
+int i2c_read(int fd, int addr, uint8_t base_reg, uint8_t *buf, size_t buf_sz)
+{
+	int status;
+	
+	int i;
+	for (i = 0; i < buf_sz; ++i) {
+		if ((status = i2c_read_reg8(fd, addr, base_reg + i, buf + i)) < 0)
+			return status;
+	}
+	
+	return i;
+}
+
+/* i2c_write: Write multiple consecutive bytes to a i2c device. Writes no more than data_sz bytes */
+int i2c_write(int fd, int addr, uint8_t base_reg, uint8_t *data, size_t data_sz)
+{
+	int status;
+	
+	int i;
+	for (i = 0; i < data_sz; ++i) {
+		if ((status = i2c_write_reg8(fd, addr, base_reg + i, data[i]) < 0)
+			return status;
+	}
+	
+	return i;
 }
